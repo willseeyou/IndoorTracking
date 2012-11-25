1bbf12a71ab3cb3dbadb7a7d0a16152ceb446c60
will <will@willpc>
2012-11-25 08:59:19 +0800

    添加GoogleMap,用于进行行走路线的绘制。

---
+++ src/cn/edu/ouc/db/DatabaseHelper.java
@@ -0,0 +1,65 @@
+package cn.edu.ouc.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+	
+	private static final int VERSION = 1;
+	// Columns
+	public static final String NUM = "num";
+	public static final String LENGTH = "length";
+	public static final String ROLL = "roll";
+	public static final String PITCH = "pitch";
+	public static final String AZIMUTH = "azimuth";
+	public static final String LAT = "lat";
+	public static final String LNG = "lng";
+	
+	private static final String DB_NAME = "indoortracking.db";
+	private static final String TBL_NAME = "track_tbl " +
+			"(" + NUM + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+			LENGTH + " FLOAT, " +
+			AZIMUTH + " FLOAT, " +
+			PITCH + " FLOAT, " +
+			ROLL + " FLOAT, " +
+			LAT + " FLOAT, " +
+			LNG + " FLOAT)";
+	private static final String CREATE_TBL_SQL = "create table " + TBL_NAME;
+	
+	private static final String TAG = "DatabaseHelper";
+
+	public DatabaseHelper(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public DatabaseHelper(Context context) {
+		this(context, DB_NAME, null, VERSION);
+	}
+	
+	public DatabaseHelper(Context context, String name) {
+		this(context, name, VERSION);
+	}
+	
+	public DatabaseHelper(Context context, String name, int version) {
+		this(context, name, null, version);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		db.execSQL(CREATE_TBL_SQL);
+		Log.i(TAG, "DatabaseHelper onCreate()......");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/cn/edu/ouc/ui/GMapActivity.java
@@ -0,0 +1,183 @@
+package cn.edu.ouc.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.os.Bundle;
+import cn.edu.ouc.R;
+import cn.edu.ouc.db.DatabaseHelper;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+
+public class GMapActivity extends MapActivity {
+
+	private MapView mMapView;  
+	private MapController mMapController;  
+	private GeoPoint centerPoint;
+	private List<GeoPoint> geoPointList;
+	MyLocationOverlay myLocationOverlay;
+	//MyTouchListener myTouchListener;
+	Path path;
+	Bitmap bitmap;
+	Bitmap bitmapDes;
+	DatabaseHelper mHelper;
+	SQLiteDatabase db;
+	private static final String TBL_NAME = "track_tbl";
+	Cursor c;
+	//SharedPreferences sharedPreferences;
+	@Override
+	protected void onCreate(Bundle icicle) {
+		// TODO Auto-generated method stub
+		super.onCreate(icicle);
+		setContentView(R.layout.googlemap);		
+		mHelper = new DatabaseHelper(getApplicationContext());
+		db = mHelper.getReadableDatabase();
+		mMapView = (MapView) findViewById(R.id.MapView01);  
+		bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.marker);
+		bitmapDes = BitmapFactory.decodeResource(getResources(), R.drawable.markera);
+        mMapView.setSatellite(true);   
+        //myTouchListener = new MyTouchListener();
+        mMapController = mMapView.getController();   
+        mMapView.setEnabled(true);  
+        mMapView.setClickable(true);  
+        mMapView.setBuiltInZoomControls(true);    
+        /*sharedPreferences = getSharedPreferences("LatLng", Context.MODE_PRIVATE);
+        double lat = Double.parseDouble(sharedPreferences.getString("lat", "36.16010"));
+        double lng = Double.parseDouble(sharedPreferences.getString("lng", "120.491951"));*/
+        centerPoint=new GeoPoint((int)(36.16010 * 1E6),(int)(120.491951* 1E6));
+        mMapController.animateTo(centerPoint);   
+        c = query();
+        mMapController.setZoom(20);   
+        myLocationOverlay = new MyLocationOverlay();  
+         List<Overlay> list = mMapView.getOverlays();  
+         list.clear();
+         list.add(myLocationOverlay);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		db.close();
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public Cursor query() {
+		c = db.query(TBL_NAME, null, null, null, null, null, null);
+		return c;
+	}
+	
+	class MyLocationOverlay extends Overlay  
+	{  
+	    
+		public MyLocationOverlay() {
+			super();
+			// TODO Auto-generated constructor stub
+			geoPointList = new ArrayList<GeoPoint>();
+		}
+
+		public boolean draw(Canvas canvas,MapView mapView,boolean shadow,long when)  
+	    {  
+	        super.draw(canvas, mapView, shadow);
+	        path = new Path();
+	        Paint paint = new Paint();  
+	        Point myScreenCoords = new Point();	     
+	        Point curCoords = myScreenCoords;
+	        mapView.getProjection().toPixels(centerPoint,myScreenCoords);
+	        paint.setStrokeWidth(5);  
+	        paint.setARGB(255, 0, 0, 255);  
+	        paint.setStyle(Paint.Style.STROKE);  
+	        paint.setAntiAlias(true);
+	        paint.setStrokeCap(Paint.Cap.ROUND);
+	        canvas.drawBitmap(bitmap, myScreenCoords.x, myScreenCoords.y, paint);
+	        c = query();
+	        if(c != null){
+			while(c.moveToNext()) {
+				GeoPoint temPoint = new GeoPoint((int)(c.getFloat(5) * 1E6),
+						(int)(c.getFloat(6) * 1E6));
+				geoPointList.add(temPoint);
+			}
+			c.close();
+	        if(geoPointList != null) 
+	        {
+	        for(int i = 0; i < geoPointList.size()-1; i++)
+	        	{
+	        	Point first = new Point();
+	        	mapView.getProjection().toPixels(geoPointList.get(i), first);
+	        	Point second = new Point();
+	        	mapView.getProjection().toPixels(geoPointList.get(i+1), second);
+	        	path.moveTo(first.x, first.y);
+	        	path.lineTo(second.x, second.y);
+	        	curCoords = second;
+	        	}        
+	        canvas.drawPath(path, paint);
+	        canvas.drawBitmap(bitmapDes, curCoords.x, curCoords.y, paint);
+	        geoPointList.clear();
+	        }
+	        }
+	        return true;  
+	    }  
+	}
+	
+	/*class MyTouchListener implements OnTouchListener {
+
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			// TODO Auto-generated method stub
+			GeoPoint point = mMapView.getProjection().fromPixels((int)event.getX(), 
+					(int)event.getY());
+			centerPoint = point;
+			double lat = point.getLatitudeE6();
+			double lng = point.getLongitudeE6();
+			Editor editor = sharedPreferences.edit();
+			editor.putString("lat", ((double)lat/1000000)+"");
+			editor.putString("lng", ((double)lng/1000000)+"");
+			editor.commit();
+			return false;
+		}
+		
+	}*/
+	
+	/*@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.mapmenu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+	    case R.id.startpoint:
+	    	mMapView.setOnTouchListener(myTouchListener);
+	        return true;
+	    case R.id.quit:	    	
+			finish();
+	        return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
+	}*/
+
+}
